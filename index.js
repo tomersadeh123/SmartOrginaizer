4c6b55d80fe17160f2c2c45decb8d31094df9a22
tomersadeh123 <142745615+tomersadeh123@users.noreply.github.com>
2024-05-09 12:49:22 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -7,13 +7,7 @@ const { google } = require('googleapis');
 const cors = require('cors'); // Import cors
 
 
-
 const app = express();
-app.use(express.static(__dirname)); // Serve static files from the 'public' directory
-
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, '404.html'));
-});
 const PORT = process.env.PORT || 3000;
 
 // CORS options
