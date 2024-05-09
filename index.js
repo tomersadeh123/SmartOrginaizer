106cb142426fa4ba6e450a8b85ebbeb61f04e9dc
tomersadeh123 <142745615+tomersadeh123@users.noreply.github.com>
2024-05-09 11:43:19 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -8,6 +8,44 @@ const cors = require('cors'); // Import cors
 
 
 const app = express();
+const server = http.createServer((req, res) => {
+  let filePath = '.' + req.url;
+  if (filePath === './') {
+      filePath = './register.html';
+  }
+
+  const extname = path.extname(filePath);
+  const contentType = {
+      '.html': 'text/html',
+      '.css': 'text/css',
+      '.js': 'text/javascript',
+      '.json': 'application/json',
+      '.png': 'image/png',
+      '.jpg': 'image/jpeg',
+      '.wav': 'audio/wav',
+      '.mp3': 'audio/mpeg',
+      '.svg': 'image/svg+xml',
+      '.pdf': 'application/pdf',
+      '.doc': 'application/msword'
+  }[extname] || 'application/octet-stream';
+
+  fs.readFile(filePath, (err, content) => {
+      if (err) {
+          if (err.code === 'ENOENT') {
+              fs.readFile('./404.html', (err, content) => {
+                  res.writeHead(404, { 'Content-Type': 'text/html' });
+                  res.end(content, 'utf-8');
+              });
+          } else {
+              res.writeHead(500);
+              res.end('Server Error: ' + err.code);
+          }
+      } else {
+          res.writeHead(200, { 'Content-Type': contentType });
+          res.end(content, 'utf-8');
+      }
+  });
+});
 const PORT = process.env.PORT || 3000;
 
 // CORS options
