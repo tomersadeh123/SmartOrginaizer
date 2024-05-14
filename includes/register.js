0bf7e87f1ea9db396866832c8c2c7eed77879654
tomer <tomersadeh7@gmail.com>
2024-05-14 16:30:11 +0300

    sperated into folders

--- includes/register.js
+++ includes/register.js
@@ -12,14 +12,15 @@ document.getElementById('registrationForm').addEventListener('submit', async (ev
       const response = await fetch('http://localhost:3000/register', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify(formData)
-      });
+    });
+    
   
       if (response.ok) {
         alert('User registered successfully');
-        window.location.href = 'login.html'; // Redirect to login page
+        window.location.href = 'includes/login.html'; // Redirect to login page
       } else {
         const errorData = await response.json();
         alert(`Registration failed: ${errorData.error}`);
