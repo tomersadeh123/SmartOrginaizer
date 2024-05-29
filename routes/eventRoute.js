57bfd43ca474adb4fede46e15bd0e3be597a10bb
tomersadeh123 <142745615+tomersadeh123@users.noreply.github.com>
2024-05-29 12:37:07 +0300

    Update eventRoute.js

--- routes/eventRoute.js
+++ routes/eventRoute.js
@@ -38,10 +38,10 @@ router.get('/events', async (req, res) => {
 
 
 
-
 router.post('/calculate-free-time', async (req, res) => {
   try {
     const { username } = req.body;
+
     // Read the JSON data from the file
     let userData = await fs.readFile(USERS_DATA_PATH, 'utf8');
     let usersData = JSON.parse(userData);
@@ -58,46 +58,63 @@ router.post('/calculate-free-time', async (req, res) => {
     // Get the current date and time in the Israel/Jerusalem time zone
     const israelTimezone = 'Asia/Jerusalem';
     const currentDate = moment().tz(israelTimezone);
-    const startOfWeek = currentDate.clone().startOf('day');
-    const endOfWeek = startOfWeek.clone().add(1, 'week');
 
-    // Filter out events that occur within the next week
-    const eventsThisWeek = allEvents.filter(event => {
-      const eventTime = moment(event.start.dateTime).tz(israelTimezone);
-      return eventTime.isBetween(startOfWeek, endOfWeek, null, '[]');
+    // Filter out events that start from the current date and have not ended
+    const futureEvents = allEvents.filter(event => {
+      const eventStartDate = moment(event.start.dateTime).tz(israelTimezone).startOf('day');
+      const currentDateStart = currentDate.clone().startOf('day');
+      const eventEndTime = moment(event.end.dateTime).tz(israelTimezone);
+      return eventStartDate.isSameOrAfter(currentDateStart) && eventEndTime.isAfter(currentDate);
     });
 
-    // Calculate free time between future events
+    // Calculate free time between future events for each day
     const freeTime = [];
-    let previousEventEnd = startOfWeek.hours(7);
+    let currentDayStart = currentDate.clone().startOf('day').hours(7);
+    let currentDayEnd = currentDayStart.clone().add(1, 'day').hours(21);
+    let previousEventEnd = currentDate.isAfter(currentDayStart) ? currentDate : currentDayStart;
 
-    for (const event of eventsThisWeek) {
+    for (const event of futureEvents) {
       const currentEventStart = moment(event.start.dateTime).tz(israelTimezone);
-      const timeDifference = currentEventStart.diff(previousEventEnd, 'minutes');
-
-      let adjustedPreviousEventEnd = previousEventEnd.clone();
-      let adjustedCurrentEventStart = currentEventStart.clone();
-
-      // Adjust times to fall within the 7 AM to 9 PM range
-      adjustedPreviousEventEnd = adjustTimes(adjustedPreviousEventEnd, startOfWeek, endOfWeek);
-      adjustedCurrentEventStart = adjustTimes(adjustedCurrentEventStart, startOfWeek, endOfWeek);
-
-      if (timeDifference > 0 && adjustedCurrentEventStart.isAfter(adjustedPreviousEventEnd)) {
-        freeTime.push({
-          start: adjustedPreviousEventEnd.format('YYYY-MM-DDTHH:mm:ss'),
-          end: adjustedCurrentEventStart.format('YYYY-MM-DDTHH:mm:ss'),
-          duration: timeDifference
-        });
+    
+      // Adjust currentDayStart and currentDayEnd based on current event's start time
+      if (currentEventStart.isAfter(currentDayEnd)) {
+        currentDayStart = currentEventStart.clone().startOf('day').hours(7);
+        currentDayEnd = currentDayStart.clone().add(1, 'day').hours(21);
       }
-
-      previousEventEnd = moment(event.end.dateTime).tz(israelTimezone);
-    }
+    
+      const timeDifference = currentEventStart.diff(previousEventEnd, 'minutes');
+    
+      if (currentEventStart.isBefore(currentDayEnd)) {
+        if (timeDifference > 0 && currentEventStart.isAfter(previousEventEnd)) {
+          freeTime.push({
+            start: previousEventEnd.format('YYYY-MM-DDTHH:mm:ss'),
+            end: currentEventStart.format('YYYY-MM-DDTHH:mm:ss'),
+            duration: timeDifference
+          });
+        }
+    
+        previousEventEnd = moment(event.end.dateTime).tz(israelTimezone);
+      } else {
+        if (previousEventEnd.isBefore(currentDayEnd)) {
+          freeTime.push({
+            start: previousEventEnd.format('YYYY-MM-DDTHH:mm:ss'),
+            end: currentDayEnd.format('YYYY-MM-DDTHH:mm:ss'),
+            duration: currentDayEnd.diff(previousEventEnd, 'minutes')
+          });
+        }
+    
+        currentDayStart = currentDayEnd;
+        currentDayEnd = currentDayStart.clone().add(1, 'day').hours(21);
+        previousEventEnd = currentEventStart;
+      }
+    }    
 
     // Update user's freeTime property
     usersData[userIndex].freeTime = freeTime;
 
     // Write updated user data back to the file
     await fs.writeFile(USERS_DATA_PATH, JSON.stringify(usersData, null, 2));
+
     res.json({ freeTime });
   } catch (error) {
     console.error('Error reading or parsing users data:', error);
@@ -105,20 +122,7 @@ router.post('/calculate-free-time', async (req, res) => {
   }
 });
 
-function adjustTimes(time, startOfWeek, endOfWeek) {
-  if (time.isBefore(startOfWeek.hours(7))) {
-    return startOfWeek.clone().hours(7);
-  } else if (time.isAfter(endOfWeek.hours(7))) {
-    return endOfWeek.clone().hours(21);
-  } else if (time.isAfter(time.clone().startOf('day').hours(21))) {
-    return time.clone().add(1, 'day').hours(7);
-  } else {
-    return time;
-  }
-}
-
 module.exports = router;
-
 
 
 
